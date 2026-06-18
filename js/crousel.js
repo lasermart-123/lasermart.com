fbdab29ced8ee83bd2b12dda7b8ca698bef0081b
lasermart-123 <vivek@lasermart.in>
2026-06-18 02:33:29 -0700

    Update crousel.js

--- js/crousel.js
+++ js/crousel.js
@@ -26,5 +26,38 @@ $(document).ready(function(){
             }
         ]
     });
+     $('#laserSlider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,        
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
 });
+
+//about
+/*$('#laserSlider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    infinite: true,
+    rtl: true
+});*/
+
