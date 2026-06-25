d15843a71fa53be64840f5cae0aa7995ca8b273a
lasermart-123 <vivek@lasermart.in>
2026-06-24 22:18:47 -0700

    Update crousel.js

--- js/crousel.js
+++ js/crousel.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         dots: false,
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 400,
+        autoplaySpeed: 700,
 
         responsive: [
             {
@@ -32,7 +32,7 @@ $(document).ready(function(){
         arrows: false,
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 400,
+        autoplaySpeed: 700,
 
         responsive: [
             {
@@ -55,7 +55,7 @@ $(document).ready(function(){
          arrows: false,
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 400,         
+        autoplaySpeed: 700,         
      
         responsive: [
             {
